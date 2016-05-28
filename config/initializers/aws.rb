2e82ba2bc06fb1fd43b234344cfd580227934758
Ubuntu <ubuntu@ip-172-31-16-100.ap-northeast-1.compute.internal>
2016-05-28 06:31:33 +0000

    test gitignore

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -2,8 +2,8 @@ CarrierWave.configure do |config|
 
   config.fog_credentials = {
     provider:              'AWS',                        # required
-    aws_access_key_id:     'xxx',                        # required
-    aws_secret_access_key: 'yyy',                        # required
+    aws_access_key_id:     'aaa',                        # required
+    aws_secret_access_key: 'bbb',                        # required
     region:                'ap-northeast-1',                  # optional, defaults to 'us-east-1'
 
     endpoint:              'https://s3-ap-northeast-1.amazonaws.com' # optional, defaults to nil
